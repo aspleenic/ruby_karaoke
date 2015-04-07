3b728f1dc35bf1a2a30a32cc27b7d7fe11e3e96b
PJ Hagerty <pjhagerty@gmail.com>
2015-04-07 13:44:01 -0400

    Changing deploy hook to hit build not public

--- deploy/after_bundle.rb
+++ deploy/after_bundle.rb
@@ -1,5 +1,5 @@
 if config.framework_env == 'staging'
-    run "cd #{config.release_path} && bundle exec middleman build --destination 'public' --future --drafts"
+    run "cd #{config.release_path} && bundle exec middleman build --destination 'build' --future --drafts"
 else
-    run "cd #{config.release_path} && bundle exec middleman build --destination 'public'"
+    run "cd #{config.release_path} && bundle exec middleman build --destination 'build'"
 end
