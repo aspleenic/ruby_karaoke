65990ed0c0aa14b76d5f3deff3e8600226f2fb59
PJ Hagerty <pjhagerty@gmail.com>
2015-05-18 17:43:35 +0100

    removing deploy hook

--- deploy/after_bundle.rb
+++
@@ -1,5 +0,0 @@
-if config.framework_env == 'staging'
-    run "cd #{config.release_path} && bundle exec middleman build"
-else
-    run "cd #{config.release_path} && bundle exec middleman build"
-end
